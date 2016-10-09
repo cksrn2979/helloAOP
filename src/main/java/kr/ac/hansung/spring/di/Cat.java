f1233e0fed752eec50cbad1cef3c2b27c00904e3
changoo <changoo@Changoo-PC>
2016-10-09 20:54:11 +0900

    init

---
+++ src/main/java/kr/ac/hansung/spring/di/Cat.java
@@ -0,0 +1,16 @@
+package kr.ac.hansung.spring.di;
+
+public class Cat implements AnimalType {
+	
+	private String myName;
+	
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+
+	public void sound() {
+		System.out.println("Cat name="+ myName +": " + "Meow!");		
+	}
+
+
+}
